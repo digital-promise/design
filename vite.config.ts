f856f673fb678716ad20a21775bd3443c4ed51c4
Eric Phillips <ephillips@digitalpromise.org>
2024-02-07 08:46:07 -0500

    Update Vite config
    Build in "library" mode and declare externals (React)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: false,
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'lib/main.ts'),
+      name: 'DPGDesign',
+      // the proper extensions will be added
+      fileName: 'digitalpromise-design'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react/jsx-runtime']
+    }
+  }
 })
