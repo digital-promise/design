64f21e07931cf3b3486cd198cd41769c107ddc5f
Eric Phillips <ephillips@digitalpromise.org>
2025-03-11 12:17:54 -0400

    Update deps, migrate to Storybook 8

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -1,20 +1,20 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import { Button } from './Button';
+import { Button } from "./Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: 'Button',
+  title: "Button",
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'centered',
+    layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   args: {
-    disabled: false
-  }
+    disabled: false,
+  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -23,13 +23,13 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
   args: {
-    label: 'Button',
+    label: "Button",
   },
 };
 
 export const Danger: Story = {
   args: {
-    label: 'Dangerous action!',
-    danger: true
-  }
-}
+    label: "Dangerous action!",
+    danger: true,
+  },
+};
