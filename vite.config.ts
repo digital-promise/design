c7349dbe88fc83b4661e1543c08bd1c78c2881a8
Eric Phillips <ephillips@digitalpromise.org>
2025-03-11 12:36:51 -0400

    Add Badge Engine style dictionary, Tailwind configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   publicDir: false,
   build: {
     lib: {
