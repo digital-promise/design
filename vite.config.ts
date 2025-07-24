2ba8d34f4829c74a4f1c9402b3e2842b906e4180
Eric Phillips <ephillips@digitalpromise.org>
2025-07-24 12:16:21 -0400

    Fix preview CSS

--- vite.config.ts
+++ vite.config.ts
@@ -4,31 +4,30 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    {
-      ...tailwindcss(),
-      apply: "serve"
+export default defineConfig(({ mode, command }) => {
+  return {
+    plugins: [
+      react(),
+      mode === "development" && command === "serve" ? tailwindcss() : false,
+    ],
+    publicDir: false,
+    build: {
+      cssCodeSplit: true,
+      lib: {
+        // Could also be a dictionary or array of multiple entry points
+        entry: [
+          resolve(__dirname, "src/index.ts"),
+          resolve(__dirname, "src/main.css"),
+        ],
+        name: "DPGDesign",
+        // the proper extensions will be added
+        fileName: "digitalpromise-design",
+      },
+      rollupOptions: {
+        // make sure to externalize deps that shouldn't be bundled
+        // into your library
+        external: ["react", "react/jsx-runtime"],
+      },
     },
-  ],
-  publicDir: false,
-  build: {
-    cssCodeSplit: true,
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: [
-        resolve(__dirname, "src/index.ts"),
-        resolve(__dirname, "src/main.css"),
-      ],
-      name: "DPGDesign",
-      // the proper extensions will be added
-      fileName: "digitalpromise-design",
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ["react", "react/jsx-runtime"],
-    },
-  },
+  };
 });
