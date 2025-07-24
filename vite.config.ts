d2edc0bc9e858dd086691250c5eb7c83145f1fda
Eric Phillips <ephillips@digitalpromise.org>
2025-07-24 02:40:32 -0400

    Change Vite entry to index Typescript file

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'lib/main.ts'),
+      entry: resolve(__dirname, 'src/index.ts'),
       name: 'DPGDesign',
       // the proper extensions will be added
       fileName: 'digitalpromise-design'
