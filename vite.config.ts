b7ab459115750a665eee2ae125c6ebccd93a0e0e
Eric Phillips <ephillips@digitalpromise.org>
2025-07-24 03:14:33 -0400

    Build main Tailwind theme file

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,34 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    {
+      ...tailwindcss(),
+      apply: "serve"
+    },
+  ],
   publicDir: false,
   build: {
+    cssCodeSplit: true,
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'DPGDesign',
+      entry: [
+        resolve(__dirname, "src/index.ts"),
+        resolve(__dirname, "src/main.css"),
+      ],
+      name: "DPGDesign",
       // the proper extensions will be added
-      fileName: 'digitalpromise-design'
+      fileName: "digitalpromise-design",
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['react', 'react/jsx-runtime']
-    }
-  }
-})
+      external: ["react", "react/jsx-runtime"],
+    },
+  },
+});
