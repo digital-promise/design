3f216706edc44fdd5bbce72154fc85d953553d3a
Eric Phillips <ephillips@digitalpromise.org>
2025-09-17 12:31:11 -0400

    Merge Storybook dev, production overrides including Tailwind plugin

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,16 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig, type UserConfig, type PluginOption } from "vite";
+
+const viteReactEntryPlugin = {
+  name: "fix-vite-inject-mocker-entry-path",
+  enforce: "post",
+  transformIndexHtml(html) {
+    return html.replace(
+      /src="\/vite-inject-mocker-entry\.js"/,
+      `src=".\/vite-inject-mocker-entry.js"`
+    );
+  },
+} as const satisfies PluginOption;
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -20,28 +32,19 @@ const config: StorybookConfig = {
     reactDocgen: "react-docgen-typescript",
   },
 
-  viteFinal: (config, { configType }) => {
-    /**
-     * Workaround for absolute path used in React Vite plugin
-     * See: https://github.com/storybookjs/storybook/issues/32428
-     */
-    if (configType === "PRODUCTION") {
-      const basePath = "/design";
-      config.base = basePath;
+  viteFinal: async (config, { configType }) => {
+    const tailwindcss = await import("@tailwindcss/vite");
+    const isProductionBuild = configType === "PRODUCTION";
+    const storybookOverrides = {
+      base: isProductionBuild ? "/design" : undefined,
+      plugins: [
+        tailwindcss.default(),
+        isProductionBuild ? viteReactEntryPlugin : false,
+      ],
+    } as const satisfies UserConfig;
 
-      config.plugins = config.plugins ?? [];
-      config.plugins.push({
-        name: "fix-vite-inject-mocker-entry-path",
-        enforce: "post",
-        transformIndexHtml(html) {
-          return html.replace(
-            /src="\/vite-inject-mocker-entry\.js"/,
-            `src=".\/vite-inject-mocker-entry.js"`
-          );
-        },
-      });
-    }
-    return config;
+    return mergeConfig(config, storybookOverrides);
   },
 };
+
 export default config;
