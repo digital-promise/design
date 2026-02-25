9db9b574bb97df29105292dede5667d1846c60f2
Eric Phillips <ephillips@digitalpromise.org>
2026-02-25 21:07:24 +0100

    Move main index file into components directory

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+import { default as Button } from "./button";
+import { default as Logo } from "./logo";
+import { default as Pagination } from "./pagination";
+import { default as Search } from "./search";
+import { default as Table } from "./table";
+export type { TableColumn } from "./table";
+
+export { Button, Logo, Pagination, Search, Table };
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { default as Button } from "./components/button";
-import { default as Logo } from "./components/logo";
-import { default as Pagination } from "./components/pagination";
-import { default as Search } from "./components/search";
-import { default as Table } from "./components/table";
-export type { TableColumn } from "./components/table";
-
-export { Button, Logo, Pagination, Search, Table };
