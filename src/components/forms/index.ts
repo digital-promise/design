8f406ee64f086e3426ab75419305186eecd48492
Eric Phillips <ephillips@digitalpromise.org>
2026-02-25 21:07:24 +0100

    Move main index file into components directory

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -1,10 +1,9 @@
 import { Checkbox } from "./checkbox";
 import { Dropzone } from "./dropzone";
-import { Editor } from "./editor";
 import { Form } from "./form";
 import { FormField } from "./form-field";
 import { Select } from "./select";
 import { TextInput } from "./text-input";
 import { Textarea } from "./textarea";
 
-export { Checkbox, Dropzone, Editor, Form, FormField, Select, Textarea, TextInput };
+export { Checkbox, Dropzone, Form, FormField, Select, Textarea, TextInput };
