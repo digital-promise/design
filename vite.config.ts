e4f15f45ee311b0e2750bf80f19332d6f362fad5
Eric Phillips <ephillips@digitalpromise.org>
2026-02-26 20:41:33 +0100

    Generate and declare types for module

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
@@ -15,7 +16,7 @@ export default defineConfig(({ command }) => {
       environment: "jsdom",
       setupFiles: ["./src/testing.ts"],
     },
-    plugins: [react()],
+    plugins: [react(), dts()],
     resolve: {
       alias: useNextNavigationShim
         ? {
