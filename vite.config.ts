1ff60cb9d3655554a0eac9819a557d2aa818f4f4
Eric Phillips <ephillips@digitalpromise.org>
2026-02-26 11:00:57 +0100

    Remove unused parameter

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode, command }) => {
+export default defineConfig(({ command }) => {
   const useNextNavigationShim =
     command === "serve" || process.env.STORYBOOK === "true";
 
