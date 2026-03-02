e55d26564cc6539296fcf3573c32c5a65184f38d
Eric Phillips <ephillips@digitalpromise.org>
2026-03-02 17:56:44 +0100

    Export Icon and IconButton components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,9 @@
 import { default as Button } from "./button";
+import { Icon, IconButton } from "./icon";
 import { default as Logo } from "./logo";
 import { default as Pagination } from "./pagination";
 import { default as Search } from "./search";
 import { default as Table } from "./table";
 export type { TableColumn } from "./table";
 
-export { Button, Logo, Pagination, Search, Table };
+export { Button, IconButton, Icon, Logo, Pagination, Search, Table };
